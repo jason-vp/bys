422bc285e7470cd35d9fd00e0b2db2b96c88f3ff
Carlos Rey Planelles <carlosreyplanelles@gmail.com>
2014-04-17 20:25:56 +0200

    Categorias+subcategorias

---
+++ BySLib/CAD/SubcategoriaCAD.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BySLib.EN
+namespace BySLib.CAD
+{
+    class SubcategoriaCAD
+    {
+                private const string ConnectionString = "data source=.\\SQLEXPRESS;Integrated Security=SSPI;AttachDBFilename=|DataDirectory|\\Videoclub.mdf;User Instance=true";
+        private SubcategoriaEN cat;
+
+        public SubcategoriaCAD(SubcategoriaEN cat)
+        {
+            this.cat = cat;
+        }
+   
+        public SubcategoriaEN ObtenerPorID(int id)
+        {
+
+        }
+
+        public void Actualizar()
+        {
+
+        }
+
+        public CategoriaEN[] listarSubcategoria()
+        {
+
+        }
+
+        public void Insertar(SubcategoriaEN nuevaCat)
+        {
+
+        }
+
+        public void BorrarCat(int id)
+        {
+
+        }
+    }
+}
---
+++ BySLib/EN/CategoriaEN.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace BySLib.EN
+{
+    class CategoriaEN
+    {
+        #region Private Properties
+        private int id;
+        private string nombre = "";
+        #endregion
+
+        public CategoriaEN(int id, string nombre)
+        {
+            this.id = id;
+            this.nombre = nombre;
+        }
+
+        public int GetId()
+        {
+            return id;
+        }
+
+        public string GetNombre()
+        {
+            return nombre;
+        }
+
+        public void setId(int id)
+        {
+            this.id = id;
+        }
+
+        public void setNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+
+
+        public void Insertar(CategoriaEN nuevaCat)
+        {
+
+        }
+
+        public void Actualizar()
+        {
+
+        }
+
+        public void BorrarCat(int id)
+        {
+
+        }
+    }
+}
