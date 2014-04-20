d0d13540339e98d87f2008fb407a64bbd9cb4c7e
theboss88 <ymundo69@gmail.com>
2014-04-20 20:40:03 +0200

    PoblacionEN,PoblacionCAD,ProvinciaEN,ProvinciaCAD

---
+++ BySLib/CAD/ProvinciaCAD.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class ProvinciaCAD{
+
+   ArrayList listar = new ArrayList(); 
+   public string conexion;
+   public bool insertarProvincia(ProvinciaEN pro){}
+   public void ActualizarProvincia(ProvinciaEN pro){}
+   public void BorrarProvincia (int id){}
+   public ArrayList listarProvincia(){}
+}
