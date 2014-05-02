311bf42a6bd49a3c4477d99ea3bcc30d1a9a469d
Carlos Rey Planelles <carlosreyplanelles@gmail.com>
2014-05-02 12:31:46 +0200

    Comentarios de categoria y subcategoria

--- BySLib/CAD/SubcategoriaCAD.cs
+++ BySLib/CAD/SubcategoriaCAD.cs
@@ -2,40 +2,75 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using BySLib.EN
+using BySLib.EN;
 namespace BySLib.CAD
 {
+    /// <summary>
+    /// Esta clase representa la capa de acceso a datos del usuario
+    /// </summary>
     class SubcategoriaCAD
     {
-                private const string ConnectionString = "data source=.\\SQLEXPRESS;Integrated Security=SSPI;AttachDBFilename=|DataDirectory|\\Videoclub.mdf;User Instance=true";
+        /// <summary>
+        /// Constante que indica los parámetros de conexión con la DB
+        /// </summary>
+        //private const string ConnectionString = "Data Source=Videoclub.sqlite;Version=3;New=False;Compress=True;";
+        private const string ConnectionString = "data source=.\\SQLEXPRESS;Integrated Security=SSPI;AttachDBFilename=|DataDirectory|\\Videoclub.mdf;User Instance=true";
+
+        /// <summary>
+        /// Variable privada que representa la Entidad de Negocio de la subcategoria
+        /// </summary>
         private SubcategoriaEN cat;
 
+        /// <summary>
+        /// Constructor por parametros
+        /// </summary>
+        /// <param name="cat">Entidad de negocio de l anueva categoria</param>
         public SubcategoriaCAD(SubcategoriaEN cat)
         {
             this.cat = cat;
         }
-   
+
+        /// <summary>
+        /// Devuelve la subcategoria cuyo id se corresponde con el que se pasa por parametro
+        /// </summary>
+        /// <param name="id">id de la subcategoria</param>
+        /// <returns>subcategoria con el mismo id que el pasado por parametro</returns>
         public SubcategoriaEN ObtenerPorID(int id)
         {
 
         }
 
+        /// <summary>
+        /// Actualiza la BD
+        /// </summary>
         public void Actualizar()
         {
 
         }
 
+        /// <summary>
+        /// Devuelve una lista de todas las Subcategorias
+        /// </summary>
+        /// <returns>lista de todas las Subcategorias</returns>
         public CategoriaEN[] listarSubcategoria()
         {
 
         }
 
+        /// <summary>
+        /// Inserta una nueva Subcategoria en la BD
+        /// </summary>
+        /// <param name="nuevaCat">Entidad de negocio de la nueva Subcategoria</param>
         public void Insertar(SubcategoriaEN nuevaCat)
         {
 
         }
 
-        public void BorrarCat(int id)
+        /// <summary>
+        /// Borra de la BD la subcategoria cuyo id se corresponde con el que se pasa por parametro
+        /// </summary>
+        /// <param name="id">id subcategoria</param>
+        public void BorrarSubCat(int id)
         {
 
         }
--- BySLib/EN/CategoriaEN.cs
+++ BySLib/EN/CategoriaEN.cs
@@ -7,6 +7,9 @@ using BySLib;
 
 namespace BySLib.EN
 {
+    /// <summary>
+    /// Esta clase representa un Usuario de la aplicacion (Entidad de negocio)
+    /// </summary>
     class CategoriaEN
     {
         #region Private Properties
@@ -14,44 +17,74 @@ namespace BySLib.EN
         private string nombre = "";
         #endregion
 
+        /// <summary>
+        /// Constructor por parametros
+        /// </summary>
+        /// <param name="id">id de la categoria</param>
+        /// <param name="nombre">nombre de la categoria</param>
         public CategoriaEN(int id, string nombre)
         {
             this.id = id;
             this.nombre = nombre;
         }
 
+        /// <summary>
+        /// Getter id
+        /// </summary>
+        /// <returns>id categoria</returns>
         public int GetId()
         {
             return id;
         }
 
+        /// <summary>
+        /// Getter nombre
+        /// </summary>
+        /// <returns>nombre categoria</returns>
         public string GetNombre()
         {
             return nombre;
         }
 
+        /// <summary>
+        /// Setter id
+        /// </summary>
+        /// <param name="id">nuevo id</param>
         public void setId(int id)
         {
             this.id = id;
         }
 
+        /// <summary>
+        /// Setter nombre
+        /// </summary>
+        /// <param name="nombre">nuevo nombre</param>
         public void setNombre(string nombre)
         {
             this.nombre = nombre;
         }
 
-
-
+        /// <summary>
+        /// Inserta la nueva categoria en la BD
+        /// </summary>
+        /// <param name="nuevaCat">nueva categoria</param>
         public void Insertar(CategoriaEN nuevaCat)
         {
 
         }
 
+        /// <summary>
+        /// Actualiza la BD
+        /// </summary>
         public void Actualizar()
         {
 
         }
 
+        /// <summary>
+        /// Borra la categoria cuyo id se corresponde con el pasado por parametro 
+        /// </summary>
+        /// <param name="id">id de la categoria</param>
         public void BorrarCat(int id)
         {
 
