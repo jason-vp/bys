3ccd8d0c2f268f273d77f8a0069954210fd70be2
Carlos Rey Planelles <carlosreyplanelles@gmail.com>
2014-05-09 18:44:55 +0200

    Pagina de registro de usuario

---
+++ BySWeb/BySWeb/Registro.aspx
@@ -0,0 +1,88 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true"
+    CodeBehind="Registro.aspx.cs" Inherits="BySWeb.Registro" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+    <style type="text/css">
+        .style1
+        {
+            text-align: center;
+        }
+        .style2
+        {
+            font-size: large;
+            color: #003399;
+        }
+        .style3
+        {
+            font-size: large;
+            color: #000099;
+        }
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="PlaceholderSidebar" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="MainContent" runat="server">
+    <p>
+        <br />
+    <asp:Panel ID="PnlError" runat="server" Visible="false">
+        <asp:Label ID="lbError" runat="server"></asp:Label>
+    </asp:Panel>
+    <p class="style2">
+        <strong>Datos de usuario</strong><p>
+        Usuario:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextB_Usuario" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        Contraseña:&nbsp;
+        <asp:TextBox ID="TextB_Contraseña" runat="server" TextMode="Password"></asp:TextBox>
+        &nbsp;&nbsp;&nbsp;&nbsp; Repite contraseña:&nbsp;
+        <asp:TextBox ID="TextB_RepiteContra" runat="server" TextMode="Password"></asp:TextBox>
+    </p>
+    <p>
+        Mail:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextB_mail" runat="server" TextMode="Email"></asp:TextBox>
+    </p>
+    <p>
+        Avatar:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextB_avatar" runat="server" TextMode="Url"></asp:TextBox>
+        &nbsp;
+    </p>
+    <p class="style3">
+        <strong>Datos personales</strong></p>
+    <p>
+        Nombre:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextB_Nombre" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        Sexo:&nbsp;
+        <asp:RadioButton ID="RadioButtonHombre" runat="server" Text="Hombre" />
+        &nbsp;
+        <asp:RadioButton ID="RadioButtonMujer" runat="server" Text="Mujer" />
+        &nbsp;&nbsp;&nbsp;&nbsp; Fecha de nacimiento:&nbsp;
+        <asp:TextBox ID="TextB_FNac" runat="server" TextMode="Date"></asp:TextBox>
+    </p>
+    <p>
+        Provincia:&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextB_provincia" runat="server"></asp:TextBox>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Localidad:&nbsp;&nbsp; &nbsp;
+        <asp:TextBox ID="TextB_Localidad" runat="server" OnTextChanged="TextBox2_TextChanged"></asp:TextBox>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; CP:&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextB_CP" runat="server" OnTextChanged="TextB_CP_TextChanged" TextMode="Number"></asp:TextBox>
+    </p>
+    <p>
+        Dirección:&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Nº:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextB_num" runat="server" TextMode="Number"></asp:TextBox>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Piso:&nbsp;
+        <asp:TextBox ID="TextB_Piso" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        Teléfono:&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextB_tlf" runat="server"></asp:TextBox>
+    </p>
+    <p class="style1">
+        <asp:Button ID="Button_Enviar" runat="server" OnClick="Button_Enviar_Click" Text="Enviar" />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <input id="Reset1" type="reset" value="reset" /></p>
+    </asp:Content>
---
+++ BySWeb/BySWeb/Registro.aspx.designer.cs
@@ -0,0 +1,186 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace BySWeb {
+    
+    
+    public partial class Registro {
+        
+        /// <summary>
+        /// Control PnlError.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel PnlError;
+        
+        /// <summary>
+        /// Control lbError.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbError;
+        
+        /// <summary>
+        /// Control TextB_Usuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextB_Usuario;
+        
+        /// <summary>
+        /// Control TextB_Contraseña.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextB_Contraseña;
+        
+        /// <summary>
+        /// Control TextB_RepiteContra.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextB_RepiteContra;
+        
+        /// <summary>
+        /// Control TextB_mail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextB_mail;
+        
+        /// <summary>
+        /// Control TextB_avatar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextB_avatar;
+        
+        /// <summary>
+        /// Control TextB_Nombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextB_Nombre;
+        
+        /// <summary>
+        /// Control RadioButtonHombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton RadioButtonHombre;
+        
+        /// <summary>
+        /// Control RadioButtonMujer.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton RadioButtonMujer;
+        
+        /// <summary>
+        /// Control TextB_FNac.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextB_FNac;
+        
+        /// <summary>
+        /// Control TextB_provincia.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextB_provincia;
+        
+        /// <summary>
+        /// Control TextB_Localidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextB_Localidad;
+        
+        /// <summary>
+        /// Control TextB_CP.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextB_CP;
+        
+        /// <summary>
+        /// Control TextBox2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+        
+        /// <summary>
+        /// Control TextB_num.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextB_num;
+        
+        /// <summary>
+        /// Control TextB_Piso.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextB_Piso;
+        
+        /// <summary>
+        /// Control TextB_tlf.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextB_tlf;
+        
+        /// <summary>
+        /// Control Button_Enviar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button_Enviar;
+    }
+}
