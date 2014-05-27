fd0a919d40729779463bb128990fc3b24bedb13b
Mario Martínez Alfonso <mario.maralf@gmail.com>
2014-05-27 18:56:22 +0200

    Hay muchas cosas. Agregado validaciones y cosas de AJAX (quita grasas)

--- BySWeb/BySWeb/Registro.aspx.cs
+++ BySWeb/BySWeb/Registro.aspx.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using BySLib.EN;
+using BySLib.AUXILIAR;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Configuration;
 
 namespace BySWeb
 {
@@ -11,6 +14,8 @@ namespace BySWeb
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+
+
             try
             {
                 lbError.Text="";
@@ -46,8 +51,103 @@ namespace BySWeb
 
         protected void TextB_CP_TextChanged(object sender, EventArgs e)
         {
+            if (TextB_CP.Text.Length == 5){
 
+               //llamada a la funcion AJAX
+                rellenaLocProv(Int32.Parse(TextB_CP.Text));
+            }
+            
         }
 
+        protected void ComprobarUsuario(object sender, ServerValidateEventArgs e)
+        {
+            string nombre = e.Value;
+
+            if (!Validacion.isNombre(nombre))
+            {
+                e.IsValid = false;
+            }
+        }
+
+        protected void ComprobarTelefono(object sender, ServerValidateEventArgs e)
+        {
+            string telefono = e.Value;
+
+            if (!Validacion.isTelefono(telefono))
+            {
+                e.IsValid = false;
+            }
+        }
+
+        protected void ComprobarDireccion(object sender, ServerValidateEventArgs e)
+        {
+            string direccion = e.Value;
+
+            if (!Validacion.isDireccion(direccion))
+            {
+                e.IsValid = false;
+            }
+        }
+
+        protected void ComprobarCpostal(object sender, ServerValidateEventArgs e)
+        {
+            string cp = e.Value;
+
+            if (!Validacion.isCpostal(cp))
+            {
+                e.IsValid = false;
+            }
+        }
+
+        protected void ComprobarEmail(object sender, ServerValidateEventArgs e)
+        {
+            string email = e.Value;
+
+            if (!Validacion.isEmail(email))
+            {
+                e.IsValid = false;
+            }
+        }
+
+        protected void ComprobarPassword(object sender, ServerValidateEventArgs e)
+        {
+            string pass = e.Value;
+
+            if (!Validacion.isPassword(pass))
+            {
+                e.IsValid = false;
+            }
+        }
+
+        protected void ComprobarLocalidad(object sender, ServerValidateEventArgs e)
+        {
+            string localidad = e.Value;
+
+            if (localidad == "")
+            {
+                e.IsValid = false;
+            }
+        }
+
+        protected void ComprobarProvincia(object sender, ServerValidateEventArgs e)
+        {
+            string provincia = e.Value;
+
+            if (provincia == "0")
+            {
+                e.IsValid = false;
+            }
+        }
+    pro
+        protected void rellenaLocProv(int CP){
+        
+            private List<P> customers; 
+        }
+        /**
+        protected void list_provincia_DataBound(object sender, EventArgs e)
+        {
+            list_provincia.Items.Insert(0, new ListItem("", "0"));
+        }
+        **/
     }
 }
