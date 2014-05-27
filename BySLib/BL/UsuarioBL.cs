e3472a539d7b93247471d2ed05a1b3010b722ca8
arturo00 <apb35@alu.ua.es>
2014-05-27 12:50:51 +0200

    cambios en las entidades

---
+++ BySLib/BL/UsuarioBL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BySLib.CAD;
+using BySLib.EN;
+using BySLib.LINQ;
+
+namespace BySLib.BL
+{
+    class UsuarioBL
+    {
+
+
+
+    }
+}
