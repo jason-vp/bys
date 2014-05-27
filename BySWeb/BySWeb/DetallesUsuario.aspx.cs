c88cd0f28b6e56c997b52eb418be4c8af2423a92
Carlos Rey Planelles <carlosreyplanelles@gmail.com>
2014-05-27 20:36:38 +0200

    Funciones para obtencion de los datos del usuario a partir de la id al cargar la página añadidas

--- BySWeb/BySWeb/DetallesUsuario.aspx.cs
+++ BySWeb/BySWeb/DetallesUsuario.aspx.cs
@@ -4,13 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using BySLib.EN;
 namespace BySWeb
 {
     public partial class DetallesUsuario : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            int id = Int32.Parse(Request.QueryString["id"]);
+            UsuarioEN user = new UsuarioEN(id);
+            nickDetUsuario.Text = user.Nick;
+            emailDetUsuario.Text = user.Mail;
+            PoblacionEN pob= new PoblacionEN(user.CodigoPostal, user.Poblacion);
+            localidadDetUsuario.Text = pob.Nombre;
+            puntuacionDetallesUsuario.Text = user.Puntacion;
+            userLogo.ImageUrl = user.rutaImg;
+
+ 
 
         }
     }
--- BySWeb/BySWeb/MiUsuario.aspx.cs
+++ BySWeb/BySWeb/MiUsuario.aspx.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using BySLib.EN;
 namespace BySWeb
 {
     public partial class MiUsuario : System.Web.UI.Page
@@ -13,6 +13,22 @@ namespace BySWeb
         {
             try
             {
+                int id = Int32.Parse(Request.QueryString["id"]);
+                UsuarioEN user=new UsuarioEN(id);
+                Nombre.Text=user.Nombre;
+                Direccion.Text=user.Direccion;
+                PoblacionEN pob= new PoblacionEN(user.CodigoPostal, user.Poblacion);
+                ProvinciaEN prov = new ProvinciaEN(pob.Cod_provincia);
+                Provincia.Text=prov.nom_prov;
+                Localidad.Text = pob.Nombre;
+                CP.Text = user.CodigoPostal;
+                Telefono.Text = user.Telf;
+                Usuario.Text = user.Nick;
+                Email.Text = user.Mail;
+                Label1.Text = user.Puntacion;
+                Contraseña_Actual.Text = user.Password;
+                Credito.Text = user.Credito;
+
                 lbError.Text = "";
                 PnlError.Visible = false;
             }
