26f7e6ba04ea8c4e31ffc4d70145dd6b39a57f9a
jason-vp <jason_vp8@hotmail.com>
2014-05-28 18:28:14 +0200

    Implementado formulario logeo pagina master

--- BySWeb/BySWeb/Site.Master.cs
+++ BySWeb/BySWeb/Site.Master.cs
@@ -4,6 +4,12 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Text;
+using System.Security.Cryptography;
+using BySLib.AUXILIAR;
+using BySLib.BL;
+using System.Web.Security;
+
 
 namespace BySWeb
 {
@@ -12,11 +18,42 @@ namespace BySWeb
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            if (Session["LoggedIn"] == null)
+            {
+                pnlLogin.Visible = true;
+                pnlLogin.Enabled = true;
+                pnlLStatus.Visible = false;
+                pnlLStatus.Enabled = false;
+
+            }
+            else
+            {
+                pnlLogin.Visible = false;
+                pnlLogin.Enabled = false;
+                pnlLStatus.Visible = true;
+                pnlLStatus.Enabled = true;
+            }
+
+
         }
 
         protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
         {
+            bool correcto=false;
+            correcto=UsuarioBL.Validate(Login1.UserName, PasswordHash.CreateHash(Login1.Password));
+
+            if (correcto)
+            {
+                Login1.FailureText = "Usuario o contraseña incorrecta.";
+            }
+            else
+            {
+                FormsAuthentication.RedirectFromLoginPage(Login1.UserName, Login1.RememberMeSet);
+            }
+
+            
 
         }
+        
     }
 }
--- BySWeb/BySWeb/Site.Master.designer.cs
+++ BySWeb/BySWeb/Site.Master.designer.cs
@@ -31,6 +31,15 @@ namespace BySWeb {
         protected global::System.Web.UI.HtmlControls.HtmlForm Form1;
         
         /// <summary>
+        /// Control pnlLogin.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pnlLogin;
+        
+        /// <summary>
         /// Control Login1.
         /// </summary>
         /// <remarks>
@@ -38,6 +47,33 @@ namespace BySWeb {
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Login Login1;
+        
+        /// <summary>
+        /// Control pnlLStatus.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pnlLStatus;
+        
+        /// <summary>
+        /// Control loginName.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LoginName loginName;
+        
+        /// <summary>
+        /// Control loginStatus.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LoginStatus loginStatus;
         
         /// <summary>
         /// Control NavigationMenu.
