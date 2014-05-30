e4e411c62845b18a21e715c4f1cd66dfc960fded
arturo00 <apb35@alu.ua.es>
2014-05-30 19:05:44 +0200

    Añadidos ProvinciaBL y PoblacionBL

---
+++ BySLib/BL/ProvinciaBL.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BySLib.LINQ;
+using BySLib.EN;
+
+namespace BySLib.BL
+{
+    class ProvinciaBL
+    {
+
+
+        internal static Provincia ConvertFromEN(ProvinciaEN prod)
+        {
+            return new Provincia()
+            {
+                cod_prov = prod.Cod_provincia,              
+                nom_prov = prod.Nombre
+
+            };
+        }
+
+        private static ProvinciaEN ConvertToEN(Provincia prod)//revisar
+        {
+            return new ProvinciaEN()
+            {
+                Cod_provincia = prod.cod_prov,
+                Nombre = prod.nom_prov
+                
+
+
+            };
+        }
+
+
+
+    }
+}
