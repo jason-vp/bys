fccb3f389dd61254c33e6aad777fe979c7042647
arturo00 <apb35@alu.ua.es>
2014-05-30 18:54:32 +0200

    Añadidos CategoriaBL y SubcategoriaBL

---
+++ BySLib/BL/SubcategoriaBL.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BySLib.LINQ;
+using BySLib.EN;
+
+namespace BySLib.BL
+{
+    class SubcategoriaBL
+    {
+
+        #region Getting Data
+
+        private static SubcategoriaEN GetById(string p_dbCnxStr, int p_id) { 
+        
+            using (BySBDDataContext cnx = DataContextManager.GetOpenedContext(p_dbCnxStr)){
+                return SubcategoriaBL.ConvertToSubEN(SubcategoriaCAD.GetById(cnx, p_id));
+            
+            }
+        
+        
+        }    
+        #endregion
+
+        #region Convert To EN
+
+        private static SubcategoriaEN ConvertToSubEN(Subcategoria p_sub)
+        {
+            //#region Check Parameters
+
+            //ParameterChecker.CheckNullParameter(MethodBase.GetCurrentMethod(),
+            //    p_consume, 1, MemberInfoGetting.GetMemberName(() => p_consume));
+
+            //#endregion
+
+            return new SubcategoriaEN()
+            {
+                Id = p_sub.id,
+                Nombre = p_sub.nombre,
+                Padre = p_sub.cat_padre 
+            };
+
+        }
+
+        private static Subcategoria ConvertFromSubEN(SubcategoriaEN p_sub)
+        {
+            //#region Check Parameters
+
+            //ParameterChecker.CheckNullParameter(MethodBase.GetCurrentMethod(),
+            //    p_consume, 1, MemberInfoGetting.GetMemberName(() => p_consume));
+
+            //#endregion
+
+            return new Subcategoria()
+            {
+                id = p_sub.Id,
+                nombre = p_sub.Nombre,
+                cat_padre = p_sub.Padre
+            };
+
+        }
+
+
+        #endregion
+
+
+
+    }
+}
